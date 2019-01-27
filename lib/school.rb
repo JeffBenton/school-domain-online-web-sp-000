8086c3c96126feb4167af3e2f03b8810274bc7cc
Jeff Benton <sikletz@gmail.com>
2019-01-27 04:40:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if !roster[grade]
+      roster[grade] = [name]
+    else
+      roster[grade] << name
+    end
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    keys = @roster.keys.sort
+    str = "{"
+    keys.each do |x|
+      str += "#{x}=>"
+  end
+end
